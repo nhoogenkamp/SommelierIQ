9827b65f3143f4a39d855173737696e47b2e1214
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-05 18:36:28 +0100

    Fixed CORS error

--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from routes.wines import get_tables, get_wines
+from flask_cors import CORS
+# Ran into CORS error:
+#https://www.google.com/search?q=from+flask_cors+import+cors&oq=from+flask_cors+import+CORS&gs_lcrp=EgZjaHJvbWUqDQgAEAAYkQIYgAQYigUyDQgAEAAYkQIYgAQYigUyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yDQgHEAAYhgMYgAQYigUyBwgIEAAY7wUyBwgJEAAY7wXSAQcyNzBqMGo5qAIGsAIB8QX1TVs-UXY3Vg&sourceid=chrome&ie=UTF-8
 
 app = Flask(__name__)
+CORS(app)
 
 # https://www.newline.co/@goatandsheep/python-dotenv-managing-your-environment-variables-with-ease--ce4fb62d
 # Using .env file to store secret password etc.
