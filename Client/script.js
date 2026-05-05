9d2997d0eaf09ac7530b4dd83bf76644e5014ac7
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-05 19:04:08 +0100

    presenting wine list in elements

--- Client/script.js
+++ Client/script.js
@@ -1,11 +1,28 @@
 //https://stackoverflow.com/questions/57891275/simple-fetch-get-request-in-javascript-to-a-flask-server 
 
+// display in table :https://www.youtube.com/watch?v=eS-FVnhjvEQ
+
 function getWines() {
     const url = 'http://localhost:8080/getWines'
     fetch(url)
     .then(response => response.json())  
     .then(json => {
-        console.log(json);
-        document.getElementById("demo").innerHTML = JSON.stringify(json)
+
+        let out = "";
+
+        json.forEach(element => {
+            out += `
+                <tr>
+                    <td>${element.year}</td>
+                    <td>${element.name}</td>
+                    <td>${element.wine_type}</td>
+                    <td>${element.grape}</td>
+                    <td>${element.country}</td>
+                    <td>${element.region}</td>
+                    <td>${element.price}</td>
+                </tr>
+            `;            
+        });
+        document.getElementById("demo").innerHTML = out;
     })
 }
