106376d290f884646e472d4b209c0957068d3291
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-05 16:40:09 +0100

    Create connection with Railway: https://www.youtube.com/watch?v=14HTiBQEQ9M

--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask,jsonify
+import mysql.connector
+
+
 app = Flask(__name__)
-@app.route("/")#URL leading to method
-def hello(): # Name of the method
- return("Hello World!") #indent this line
-if __name__ == "__main__":
- app.run(host='0.0.0.0', port='8080') # indent this line
+
+con=mysql.connector.connect(
+    host='trolley.proxy.rlwy.net',
+    port=39957,
+    database='railway',
+    user='root',
+    password='fnKEhqNMePaAhEdEzsEJkhEZNiDgANTw'
+)
+@app.route('/getTable', methods=['GET'])
+def get_tables():
+    cursor=con.cursor()
+    cursor.execute("SHOW TABLES;")
+    tables=cursor.fetchall()
+    cursor.close()
+    con.close()
+    table_names=[table[0] for table in tables]
+    return jsonify({"tables":table_names}),200
+
+
+if __name__=="__main__":
+    print("connecting to DB....")
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port='8080')
