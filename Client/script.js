fc9ad31c72c88aad1112cecb3de18167dcf10011
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-06 14:35:04 +0100

    grouping wines in colour and country; https://www.tutorialspoint.com/article/json-group-object-in-javascript

--- Client/script.js
+++ Client/script.js
@@ -2,11 +2,32 @@
 
 // display in table :https://www.youtube.com/watch?v=eS-FVnhjvEQ
 
+// grouping wines in colour and country; https://www.tutorialspoint.com/article/json-group-object-in-javascript
+
 function getWines() {
     const url = 'http://localhost:8080/getWines'
     fetch(url)
     .then(response => response.json())  
     .then(json => {
+
+        const grouped = json.reduce((groups, wine) => {
+
+            const colour = wine.wine_type;
+            const country = wine.country;
+
+            if (!groups[colour]) {
+                groups[colour] = {};
+            }
+
+            if (!groups[colour][country]) {
+                groups[colour][country] = [];
+            }
+
+            groups[colour][country].push(wine);
+
+            return groups;
+
+        }, {});
 
         let out = "";
 
@@ -14,6 +35,7 @@ function getWines() {
             out += `
                 <tr>
                     <td>${element.year}</td>
+                    <td>${element.bottle_type}</td>
                     <td>${element.name}</td>
                     <td>${element.wine_type}</td>
                     <td>${element.grape}</td>
