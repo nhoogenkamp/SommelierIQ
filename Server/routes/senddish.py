d931fb1ba5838dd58fe12e6bbb20386a36190426
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-07 13:44:40 +0100

    Use the data from frontend to query the database

--- Server/routes/senddish.py
+++ Server/routes/senddish.py
@@ -1,4 +1,5 @@
 from flask import request, jsonify
+from db import get_db_connection
 
 # Function handles POST request from frontend
 def send_dish():
@@ -7,6 +8,35 @@ def send_dish():
     data = request.get_json()
     # Print received data in Flask terminal
     print(data)
-    
-    # Return same data back to frontend
-    return jsonify(data), 200
+
+    # Get the value stored in the key "dish"
+    selected_dish = data["dish"]
+
+    con = get_db_connection()
+    cursor = con.cursor(dictionary=True)
+
+    # SQL query searches for matching food item : https://www.w3schools.com/python/python_mysql_where.asp
+    sql = "SELECT * FROM food_items WHERE name = %s"
+    dish = (selected_dish,)
+
+    # Execute SQL query safely to prevent SQL Injection
+    cursor.execute(sql, dish)
+
+    # Get matching food rows from database
+    foods = cursor.fetchall()
+
+    print(foods)
+
+    # Close cursor
+    cursor.close()
+
+    # Close database connection
+    con.close()
+
+    return jsonify({
+    # Original data sent from frontend
+    "received_data": data,
+
+    # Food rows returned from database
+    "foods": foods
+    }), 200 
