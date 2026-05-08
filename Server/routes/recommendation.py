649c9edae971965e97d91bc96d5646f0fe33465d
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-08 11:08:40 +0100

    Sweetness calc

--- Server/routes/recommendation.py
+++ Server/routes/recommendation.py
@@ -4,7 +4,7 @@ def calculate_match(food, wine):
     if wine["wine_type"] == food["colour_wine"]:
         total_score += wine["colour_score"]
 
-    # calculating body score, tanning score, acidity difference
+    # calculating body score, tanning score, acidity and sweetness difference
     # to ensure no negative numbers using abs() https://www.w3schools.com/python/ref_func_abs.asp
     body_difference = abs(food["body_score"]-wine["body_score"])
     total_score += 20 - body_difference
@@ -14,5 +14,8 @@ def calculate_match(food, wine):
 
     acidity_difference = abs(food["acidity_score"]-wine["acidity_score"])
     total_score += 20 - acidity_difference
+
+    sweetness_difference = abs(food["sweetness_score"]-wine["sweetness_score"])
+    total_score += 20 - sweetness_difference
 
     return total_score    
