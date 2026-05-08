d469a40650cebf6f6df2f5a74e0853fdcaa57adc
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-08 10:56:55 +0100

    body scoring

--- Server/routes/recommendation.py
+++ Server/routes/recommendation.py
@@ -4,4 +4,10 @@ def calculate_match(food, wine):
     if wine["wine_type"] == food["colour_wine"]:
         total_score += wine["colour_score"]
 
+    # calculating body score difference
+    # to ensure no negative numbers using abs() https://www.w3schools.com/python/ref_func_abs.asp
+    body_difference = abs(food["body_score"]-wine["body_score"])
+    total_score += 20 - body_difference
+
+
     return total_score    
