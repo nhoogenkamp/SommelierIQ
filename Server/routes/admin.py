329773f98a382d7844539095a2d089519470a2c7
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-16 14:00:34 +0100

    created admin function

---
+++ Server/routes/admin.py
@@ -0,0 +1,36 @@
+from flask import request, jsonify
+from db import get_db_connection
+# flask library that helps create and read passwords
+from werkzeug.security import generate_password_hash
+
+def add_admin():
+
+    # get JSON data sent from JavaScript
+    data = request.get_json()
+
+    print(data)
+
+    restaurant_id = data["restaurant_id"]
+    username = data["username"]
+    password = data["password"]
+
+    # turn normal password into hashed password
+    password_hash = generate_password_hash(password)
+
+    con = get_db_connection()
+    cursor = con.cursor(dictionary=True)
+
+    # insert admin into admins table
+    sql = "INSERT INTO admins (restaurant_id, username, password_hash) VALUES (%s, %s, %s)"
+    values = (restaurant_id, username, password_hash)
+
+    cursor.execute(sql, values)
+
+    con.commit()
+
+    cursor.close()
+    con.close()
+
+    return jsonify({
+        "message": "Admin account created"
+    }), 200
