ab51d9ab39e08ee35b4c2bcb927c922af910c4d0
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-16 14:33:54 +0100

    javascript functio for registration + html update to js

---
+++ Client/javascript/register.js
@@ -0,0 +1,42 @@
+function register_admin() {
+    
+    // getting values from html 
+    var restaurant_id = document.getElementById("restaurant_id").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // object sent to Flask backend
+    var entry = {
+        restaurant_id: restaurant_id,
+        username: username,
+        password: password
+    };
+
+     // Send POST request to Flask backend with method, body preventing browser from caching and telling flask its JSON data
+    fetch(`http://localhost:8080/addAdmin`, {
+        method: "POST",
+        body: JSON.stringify(entry),
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json"
+        })
+    })
+
+    // Check response from Flask
+    .then(function(response) {
+        if (response.status !==200) {
+            console.log(`response status was not 200: ${response.status}`);
+            return;
+        }
+        console.log("received new admin user")
+        // had to return in order to use the next .then function
+        return response.json()
+    })
+
+    .then(function(json) {
+        console.log(json);
+        document.getElementById("message").innerHTML = json.message || json.error;
+
+    });
+
+}
