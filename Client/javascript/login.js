ce7ec6ceb3db47e3721d4e5a52ed4d9bd93b25b3
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-16 15:41:18 +0100

    Create login function

---
+++ Client/javascript/login.js
@@ -0,0 +1,48 @@
+function login_admin() {
+    
+    // getting values from html 
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // object sent to Flask backend
+    var entry = {
+        username: username,
+        password: password
+    };
+
+     // Send POST request to Flask backend with method, body preventing browser from caching and telling flask its JSON data
+    fetch(`http://localhost:8080/addAdmin`, {
+        method: "POST",
+        body: JSON.stringify(entry),
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json"
+        })
+    })
+
+    // Check response from Flask
+    .then(function(response) {
+        if (response.status !==200) {
+            console.log(`response status was not 200: ${response.status}`);
+            return response.json();
+        }
+        console.log("received new admin user")
+        // had to return in order to use the next .then function
+        return response.json()
+    })
+
+    .then(function(json) {
+        console.log(json);
+        document.getElementById("message").innerHTML = json.message || json.error;
+
+        if(json.message){
+            // clearing fields
+            document.getElementById("registerForm").reset();
+
+            // redirect to dashboard page    
+            window.location.href = "dashboard.html";
+        }
+
+    });
+
+}
