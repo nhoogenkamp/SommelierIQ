49d492a8616430082fb4c272ab8baf9e5c60237e
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-16 15:58:21 +0100

    added dashboard get wines and added wineID

---
+++ Client/javascript/dashboard.js
@@ -0,0 +1,49 @@
+//https://stackoverflow.com/questions/57891275/simple-fetch-get-request-in-javascript-to-a-flask-server 
+
+// display in table :https://www.youtube.com/watch?v=eS-FVnhjvEQ
+
+function filterWines(wines) {
+    const selectedColour = document.getElementById("wineColour").value;
+    const selectedBottle = document.getElementById("bottleType").value;
+    const maxPrice = document.getElementById("maxPrice").value;
+
+    return wines.filter(wine => {
+
+        return (
+            (selectedColour === "" || wine.wine_type === selectedColour) &&
+            (selectedBottle === "" || wine.bottle_type === selectedBottle) &&
+            (maxPrice === "" || Number(wine.price) <= Number(maxPrice))
+        );
+
+    });
+}
+
+function getWines() {
+    const url = 'http://localhost:8080/getWines'
+    fetch(url)
+    .then(response => response.json())  
+    .then(json => {
+
+    const filteredWines = filterWines(json);
+
+    let out = "";
+
+    filteredWines.forEach(element => {
+        out += `
+            <tr>
+                <td>${element.wine_id}</td>
+                <td>${element.year}</td>
+                <td>${element.bottle_type}</td>
+                <td>${element.name}</td>
+                <td>${element.wine_type}</td>
+                <td>${element.grape}</td>
+                <td>${element.country}</td>
+                <td>${element.region}</td>
+                <td>${element.price}</td>
+            </tr>
+        `;
+    });
+
+    document.getElementById("wines").innerHTML = out;
+});
+}
