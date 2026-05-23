2f72f9bc5f5fee21add1ea000ccaff9d62e4d27f
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-23 17:39:20 +0100

    delete from sql function

--- Server/routes/deleteWine.py
+++ Server/routes/deleteWine.py
@@ -1,5 +1,26 @@
 from flask import request, jsonify
 from db import get_db_connection
 
-def add_wine():
-    
+def delete_wine():
+    data = request.get_json()
+    print(data)
+
+    wine_id = data["wine_id"]
+
+    con = get_db_connection()
+    cursor = con.cursor(dictionary=True)
+   
+    # insert admin into admins table
+    sql = "DELETE FROM wines WHERE wine_id = %s"
+    values = (wine_id,)
+
+    cursor.execute(sql, values)
+
+    con.commit()
+
+    cursor.close()
+    con.close()
+
+    return jsonify({
+        "message": "Wine is deleted"
+    }), 200   
