407472c6dadbcc2bc83af842c2220293962e4c35
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-05-30 15:08:44 +0100

    food menu validation

--- Server/routes/menu.py
+++ Server/routes/menu.py
@@ -1,11 +1,31 @@
 from flask import jsonify
 from db import get_db_connection
+import mysql.connector
 
 def get_food():
-    con = get_db_connection()
-    cursor = con.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM food_items;")
-    foods = cursor.fetchall()
-    cursor.close()
-    con.close()
+    # 503 error for connection
+    try:
+        con = get_db_connection()
+        cursor = con.cursor(dictionary=True)
+
+    except mysql.connector.Error as err:
+        print("Error:", err.errno)
+
+        return jsonify({
+            "error": "Could not connect with database"
+        }), 503   
+
+    try:
+        cursor.execute("SELECT * FROM food_items;")
+        foods = cursor.fetchall()
+    except mysql.connector.Error as err:
+        print("Error:", err)
+
+        return jsonify({
+            "error": "Could not get food items from database"
+        }), 500
+    
+    finally:
+        cursor.close()
+        con.close()
     return jsonify(foods), 200
